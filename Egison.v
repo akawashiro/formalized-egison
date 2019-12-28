7ed28589d88ef3c2d4d23585b05f87fa41c849b5
Akira Kawata <akirakawata1@gmail.com>
2019-12-28 22:33:49 +0900

    Start to make typing rules.

--- Egison.v
+++ Egison.v
@@ -11,22 +11,22 @@ Module Egison.
 
   Definition varid := string.
 
-   Inductive tm : Type :=
-  | tvar : varid -> tm
-  | tint : nat -> tm
-  (* | tlmb : varid -> tm -> tm *)
-  (* | tapp : tm -> tm -> tm *)
-  | ttpl : tm -> tm -> tm
-  | tcll : list tm -> tm
-  | tpair : tm -> tm -> tm
-  | tmal : tm -> tm -> (ptn * tm) -> tm
-  | tsm : tm
-  | tmtc : (list (pptn * tm * (list (dptn * tm)))) -> tm
-  | ttplmtc : tm -> tm -> tm
+   Inductive exp : Type :=
+  | evar : varid -> exp
+  | eint : nat -> exp
+  (* | tlmb : varid -> exp -> exp *)
+  (* | tapp : exp -> exp -> exp *)
+  | etpl : exp -> exp -> exp
+  | ecll : list exp -> exp
+  | epair : exp -> exp -> exp
+  | expmal : exp -> exp -> (ptn * exp) -> exp
+  | esm : exp
+  | emtc : (list (pptn * exp * (list (dptn * exp)))) -> exp
+  | etplmtc : exp -> exp -> exp
   with ptn : Type :=
   | pwld : ptn
   | pvar : varid -> ptn
-  | pval : tm -> ptn
+  | pval : exp -> ptn
   | ppair : ptn -> ptn -> ptn
  with pptn : Type :=
   | ppdol : pptn
@@ -36,50 +36,61 @@ Module Egison.
   | dpvar :  varid -> dptn
   | dppair :  dptn -> dptn -> dptn.
 
-  Definition ppex1 := ppdol : pptn.
+   Inductive ty : Type :=
+   | tint : ty
+   | ttpl : ty -> ty -> ty
+   | tcll : ty -> ty
+   | tpair : ty -> ty -> ty
+   | tmtc : ty -> ty
+   | tptn : ty -> ty
+   | tpptn : ty -> ty
+   | tdptn : ty -> ty.
 
-  Definition env := partial_map tm.
+  (* Definition ppex1 := ppdol : pptn. *)
 
-  (* Definition mlcssize (f : tm -> nat) (mcl : pptn * tm * (list (dptn * tm))) : nat := *)
+  Definition env := partial_map exp.
+  Definition tenv := partial_map ty.
+
+  (* Definition mlcssize (f : exp -> nat) (mcl : pptn * exp * (list (dptn * exp))) : nat := *)
   (*   let '(_, m1, l) := mcl in *)
   (*   max (f m1) (fold_left max (map (compose f snd) l) 0). *)
 
-  (* Fixpoint tmsize (m: tm) : nat := *)
+  (* Fixpoint expsize (m: exp) : nat := *)
   (*   match m with *)
-  (*   | tvar _ => 1 *)
-  (*   | tint _ => 1 *)
-  (*   (* | tlmb _ n => 1 + tmsize n *) *)
-  (*   (* | tapp n1 n2 => 1 + max (tmsize n1) (tmsize n2) *) *)
-  (*   | ttpl ms => 1 + fold_left max (map tmsize ms) 0 *)
-  (*   | tcll ms => 1 + fold_left max (map tmsize ms) 0 *)
-  (*   | tpair m1 m2 => 1 + max (tmsize m1) (tmsize m2) *)
-  (*   | tmal m1 m2 pts => 1 + max (max (tmsize m1) (tmsize m2)) (fold_left max (map (compose tmsize snd) pts) 0) *)
-  (*   | tsm => 1 *)
-  (*   | tmtc mcl => 1 + fold_left max (map (mlcssize tmsize) mcl) 0 *)
+  (*   | evar _ => 1 *)
+  (*   | eint _ => 1 *)
+  (*   (* | tlmb _ n => 1 + expsize n *) *)
+  (*   (* | tapp n1 n2 => 1 + max (expsize n1) (expsize n2) *) *)
+  (*   | etpl ms => 1 + fold_left max (map expsize ms) 0 *)
+  (*   | ecll ms => 1 + fold_left max (map expsize ms) 0 *)
+  (*   | epair m1 m2 => 1 + max (expsize m1) (expsize m2) *)
+  (*   | expmal m1 m2 pts => 1 + max (max (expsize m1) (expsize m2)) (fold_left max (map (compose expsize snd) pts) 0) *)
+  (*   | esm => 1 *)
+  (*   | emtc mcl => 1 + fold_left max (map (mlcssize expsize) mcl) 0 *)
   (*   end. *)
 
-  Definition mclsvalue (f : tm -> Prop) (mcl : pptn * tm * (list (dptn * tm))) :=
+  Definition mclsvalue (f : exp -> Prop) (mcl : pptn * exp * (list (dptn * exp))) :=
     let '(_, m1, l) := mcl in (f m1) /\ (List.Forall (fun m => f (snd m))) l.
 
-  Definition mclstms (mcl : pptn * tm * (list (dptn * tm))) : list tm :=
+  Definition mclsexps (mcl : pptn * exp * (list (dptn * exp))) : list exp :=
     let '(_, m1, l) := mcl in
     m1 :: map (fun m => snd m) l.
 
-  Inductive value : tm -> Prop :=
-  | vvar : forall i, value (tvar i)
-  | vint : forall i, value (tint i)
-  (* | vlmb : forall i m, value (tlmb i m) *)
-  | vtpl : forall m1 m2, value m1 -> value m2 -> value (ttpl m1 m2)
-  | vcll : forall ms, Forall value ms -> value (tcll ms)
-  | vpair : forall m1 m2, value m1 /\ value m2 -> value (tpair m1 m2)
-  | vmal : forall m1 m2 pt, value m1 -> value m2 -> value (snd pt) -> value (tmal m1 m2 pt)
-  | vmtc : forall mcls, Forall value (concat (map mclstms mcls)) -> value (tmtc mcls)
-  | vtplmtc : forall m1 m2, value m1 -> value m2 -> value (ttplmtc m1 m2).
+  Inductive value : exp -> Prop :=
+  | V_Var : forall i, value (evar i)
+  | V_Int : forall i, value (eint i)
+  | V_Tpl : forall m1 m2, value m1 -> value m2 -> value (etpl m1 m2)
+  | V_Cll : forall ms, Forall value ms -> value (ecll ms)
+  | V_Pair : forall m1 m2, value m1 /\ value m2 -> value (epair m1 m2)
+  | V_Mal : forall m1 m2 pt, value m1 -> value m2 -> value (snd pt) -> value (expmal m1 m2 pt)
+  | V_Mtc : forall mcls, Forall value (concat (map mclsexps mcls)) -> value (emtc mcls)
+  | V_Tplmtc : forall m1 m2, value m1 -> value m2 -> value (etplmtc m1 m2).
+  (* | V_Lmb : forall i m, value (tlmb i m) *)
 
   Import ListNotations.
 
-  Definition ms : Type := ((list (ptn * tm * env * tm)) * env * env).
-  Definition ma : Type := (ptn * tm * env * tm).
+  Definition ms : Type := ((list (ptn * exp * env * exp)) * env * env).
+  Definition ma : Type := (ptn * exp * env * exp).
 
   Fixpoint filtersome {A: Type} (l: list (option A)) : list A :=
     match l with
@@ -120,9 +131,9 @@ Module Egison.
     | (h1::r1, h2::r2, h3::r3, h4::r4) => (h1,h2,h3,h4) :: zip4 r1 r2 r3 r4
     end.
 
-  Definition is_tpair (t: tm) : Prop :=
+  Definition is_epair (t: exp) : Prop :=
     match t with
-    | (tpair _ _) => True
+    | (epair _ _) => True
     | _ => False
     end.
 
@@ -138,51 +149,65 @@ Module Egison.
     | _ => False
     end.
 
-  Inductive eval : (env * tm * tm)-> Prop :=
-  | evarin : forall i Gamma t, (Gamma i) = Some t -> eval (Gamma, (tvar i), t)
-  | evarout : forall i Gamma, (Gamma i) = None -> eval (Gamma, (tvar i), (tvar i))
-  | eint : forall i e, eval (e, (tint i), (tint i))
-  | etpl : forall Gamma t1 t2 v1 v2, eval (Gamma, t1, v1) -> eval (Gamma, t2, v2) -> eval (Gamma, ttpl t1 t2, ttpl v1 v2)
-  | ecll : forall e ts vs, same_length_list ts vs ->
-                      Forall eval (map (fun tpl => let '(t,v) := tpl in (e,t,v)) (zip ts vs)) -> eval (e, (tcll ts), (tcll vs))
-  | epair : forall e t1 t2 v1 v2, eval (e, t1, v1) -> eval (e, t2, v2) -> eval (e, (tpair t1 t2), (tpair v1 v2))
-  | esm : forall Gamma, eval (Gamma, tsm, tsm)
-  (* | emtc : forall Gamma (ts: (list (pptn * tm * (list (dptn * tm))))), eval Gamma ((tmtc ts), (tmtc vs)) *)
-  | etplmtc : forall Gamma t1 t2 v1 v2, eval (Gamma, t1, v1) -> eval (Gamma, t2, v2) -> eval (Gamma, ttplmtc t1 t2, ttplmtc v1 v2)
-  | etmal : forall Gamma M N p L v_v v m_m m_e Delta_v, same_length_list Delta_v v_v -> eval (Gamma, M,v) -> evalmtc Gamma N [(m_m, m_e)] -> evalms3 [[([(p,m_m,m_e,v)], Gamma, empty)]] Delta_v ->
+  Inductive typed : (tenv * exp * ty * tenv) -> Prop :=
+  | T_Var : forall Gamma s T, Gamma s = Some T -> typed (Gamma, (evar s), T, Gamma)
+  | T_Int : forall Gamma i, typed (Gamma, (eint i), tint, Gamma)
+  | T_Pair : forall Gamma e1 T1 e2 T2, typed (Gamma, e1, T1, Gamma) ->
+                                  typed (Gamma, e2, T2, Gamma) ->
+                                  typed (Gamma, (epair e1 e2), (tpair T1 T2), Gamma)
+  | T_Cll : forall Gamma es T, Forall typed (map (fun e => (Gamma,e,T,Gamma)) es) ->
+                          typed (Gamma, (ecll es), (tcll T), Gamma).
+
+  Theorem T_Int_example : typed (empty, (eint 10), tint, empty).
+  Proof.
+    econstructor.
+  Qed.
+
+  Inductive eval : (env * exp * exp)-> Prop :=
+  | E_VarIn : forall i Gamma t, (Gamma i) = Some t -> eval (Gamma, (evar i), t)
+  | E_VarOut : forall i Gamma, (Gamma i) = None -> eval (Gamma, (evar i), (evar i))
+  | E_Int : forall i e, eval (e, (eint i), (eint i))
+  | E_Tpl : forall Gamma t1 t2 v1 v2, eval (Gamma, t1, v1) -> eval (Gamma, t2, v2) -> eval (Gamma, etpl t1 t2, etpl v1 v2)
+  | E_Cll : forall e ts vs, same_length_list ts vs ->
+                      Forall eval (map (fun tpl => let '(t,v) := tpl in (e,t,v)) (zip ts vs)) -> eval (e, (ecll ts), (ecll vs))
+  | E_Pair : forall e t1 t2 v1 v2, eval (e, t1, v1) -> eval (e, t2, v2) -> eval (e, (epair t1 t2), (epair v1 v2))
+  | E_Sm : forall Gamma, eval (Gamma, esm, esm)
+  (* | emtc : forall Gamma (ts: (list (pptn * exp * (list (dptn * exp))))), eval Gamma ((emtc ts), (emtc vs)) *)
+  | E_Tplmtc : forall Gamma t1 t2 v1 v2, eval (Gamma, t1, v1) -> eval (Gamma, t2, v2) -> eval (Gamma, etplmtc t1 t2, etplmtc v1 v2)
+  | E_Expmal : forall Gamma M N p L v_v v m_m m_e Delta_v, same_length_list Delta_v v_v -> eval (Gamma, M,v) -> evalmtc Gamma N [(m_m, m_e)] -> evalms3 [[([(p,m_m,m_e,v)], Gamma, empty)]] Delta_v ->
                                                   Forall eval (map (fun t => let '(d,v) := t in (Gamma @@ d, L, v)) (zip Delta_v v_v)) ->
-                                                  eval (Gamma, (tmal M N (p, L)), tcll v_v)
+                                                  eval (Gamma, (expmal M N (p, L)), ecll v_v)
 
-  with evalmtc : env -> tm -> list (tm * env) -> Prop :=
-  | emtcsm : forall Gamma, evalmtc Gamma tsm [(tsm, Gamma)]
-  | emtcmtc : forall Gamma l, evalmtc Gamma (tmtc l) [((tmtc l), Gamma)]
-  | emtctpl : forall Gamma m1 m2 n1 n2, eval (Gamma, (ttplmtc m1 m2), (ttplmtc n1 n2)) -> evalmtc Gamma (ttplmtc m1 m2) [(n1, Gamma); (n2, Gamma)]
+  with evalmtc : env -> exp -> list (exp * env) -> Prop :=
+  | Emtc_Sm : forall Gamma, evalmtc Gamma esm [(esm, Gamma)]
+  | Emtc_Mtc : forall Gamma l, evalmtc Gamma (emtc l) [((emtc l), Gamma)]
+  | Emtc_Tpl : forall Gamma m1 m2 n1 n2, eval (Gamma, (etplmtc m1 m2), (etplmtc n1 n2)) -> evalmtc Gamma (etplmtc m1 m2) [(n1, Gamma); (n2, Gamma)]
 
-  with evaldp : dptn -> tm -> option env -> Prop :=
-  | edpvar : forall z v, value v -> evaldp (dpvar z) v (Some (z |-> v))
-  | edppair : forall p1 p2 v1 v2 g1 g2,
+  with evaldp : dptn -> exp -> option env -> Prop :=
+  | Edp_Var : forall z v, value v -> evaldp (dpvar z) v (Some (z |-> v))
+  | Edp_Pair : forall p1 p2 v1 v2 g1 g2,
       value v1 -> value v2 -> evaldp p1 v1 (Some g1) -> evaldp p2 v2 (Some g2) ->
-      evaldp (dppair p1 p2) (tpair v1 v2) (Some (g1 @@ g2))
-  | edpfail : forall t p1 p2, not (is_tpair t) -> evaldp (dppair p1 p2) t None
+      evaldp (dppair p1 p2) (epair v1 v2) (Some (g1 @@ g2))
+  | Edp_Fail : forall t p1 p2, not (is_epair t) -> evaldp (dppair p1 p2) t None
 
   with evalpp : pptn -> env -> ptn -> option ((list ptn) * env) -> Prop :=
-  | eppdol : forall g p, evalpp ppdol g p (Some ([p], empty))
-  | eppvar : forall i g m v, eval (g, m, v) -> evalpp (ppvar i) g (pval m) (Some ([], (i |-> v)))
-  | epppair : forall pp1 pp2 p1 p2 g pv1 pv2 g1 g2,
+  | Epp_Dol : forall g p, evalpp ppdol g p (Some ([p], empty))
+  | Epp_Var : forall i g m v, eval (g, m, v) -> evalpp (ppvar i) g (pval m) (Some ([], (i |-> v)))
+  | Epp_Pair : forall pp1 pp2 p1 p2 g pv1 pv2 g1 g2,
                 evalpp pp1 g p1 (Some (pv1,g1)) -> evalpp pp2 g p2 (Some (pv2,g2)) ->
                 evalpp (pppair pp1 pp2) g (ppair p1 p2) (Some ((pv1 ++ pv2), (g1 @@ g2)))
-  | eppvarfail : forall y g p, not (is_pval p) -> evalpp (ppvar y) g p None
-  | epppairfail : forall pp1 pp2 p g, not (is_ppair p) -> evalpp (pppair pp1 pp2) g p None
+  | Epp_VarFail : forall y g p, not (is_pval p) -> evalpp (ppvar y) g p None
+  | Epp_PairFail : forall pp1 pp2 p g, not (is_ppair p) -> evalpp (pppair pp1 pp2) g p None
 
   with evalms1 : ((list ms) * option env * option (list ms)) -> Prop :=
-  | ems1nil : evalms1 ([], None, None)
-  | ems1anil : forall sv g d, evalms1 ((([],g,d)::sv), (Some d), (Some sv))
-  | ems1 : forall p m mg v av g d sv avv d1,
+  | Ems1_Nil : evalms1 ([], None, None)
+  | Ems1_ANil : forall sv g d, evalms1 ((([],g,d)::sv), (Some d), (Some sv))
+  | Ems1 : forall p m mg v av g d sv avv d1,
         evalma (g @@ d) (p,m,mg,v) avv d1 ->
         evalms1 ((((p,m,mg,v)::av, g, d)::sv), None, (Some ((map (fun ai => (ai ++ av, g, d @@ d1)) avv) ++ sv)))
 
   with evalms2 : (list (list ms)) -> (list env) -> (list (list ms)) -> Prop :=
-  | ems2 : forall svv gvv svv1 gvv1 svv2,
+  | Ems2 : forall svv gvv svv1 gvv1 svv2,
       same_length_list3 svv gvv svv1 ->
       Forall evalms1 (zip3 svv gvv svv1) ->
       (filtersome gvv) = gvv1 ->
@@ -190,27 +215,27 @@ Module Egison.
       evalms2 svv gvv1 svv2
 
   with evalms3 : (list (list ms)) -> (list env) -> Prop :=
-  | ems3nil : evalms3 [[]] []
-  | ems3 : forall svv gv svv1 dv gdv, evalms2 svv gv svv1 -> evalms3 svv1 dv -> gdv = gv ++ dv ->
+  | Ems3_nil : evalms3 [[]] []
+  | Ems3 : forall svv gv svv1 dv gdv, evalms2 svv gv svv1 -> evalms3 svv1 dv -> gdv = gv ++ dv ->
                              evalms3 svv gdv
 
   with evalma : env -> ma -> list (list ma) -> env -> Prop :=
-  | emasome : forall x g v d, evalma g (pvar x, tsm, d, v) [[]] (x |-> v)
-  | emappfail : forall p g pp m sv pv d v avv g1,
-      evalpp pp g p None -> evalma g (p,(tmtc pv),d,v) avv g1 ->
-      evalma g (p,tmtc ((pp,m,sv)::pv),d,v) avv g1
-  | emadpfail : forall p g pp m dp n sv pv d v pv1 d1 avv g1,
+  | Ema_Some : forall x g v d, evalma g (pvar x, esm, d, v) [[]] (x |-> v)
+  | Ema_Ppfail : forall p g pp m sv pv d v avv g1,
+      evalpp pp g p None -> evalma g (p,(emtc pv),d,v) avv g1 ->
+      evalma g (p,emtc ((pp,m,sv)::pv),d,v) avv g1
+  | Ema_Dpfail : forall p g pp m dp n sv pv d v pv1 d1 avv g1,
       evalpp pp g p (Some (pv1, d1)) -> evaldp dp v None ->
-      evalma g (p, tmtc ((pp,m,sv)::pv),d,v) avv g1 ->
-      evalma g (p, tmtc ((pp,m,(dp,n)::sv)::pv),d,v) avv g1
-  | ema : forall p Gamma pp M dp N sigma_v Delta v phi1_v p1_v Delta1 Delta2 v1_vv m1_v,
+      evalma g (p, emtc ((pp,m,sv)::pv),d,v) avv g1 ->
+      evalma g (p, emtc ((pp,m,(dp,n)::sv)::pv),d,v) avv g1
+  | Ema : forall p Gamma pp M dp N sigma_v Delta v phi1_v p1_v Delta1 Delta2 v1_vv m1_v,
       evalpp pp Gamma p (Some (p1_v, Delta1)) ->
       evaldp dp v (Some Delta2) ->
-      eval (Delta @@ Delta1 @@ Delta2, N, tcll v1_vv) ->
+      eval (Delta @@ Delta1 @@ Delta2, N, ecll v1_vv) ->
       evalmtc Gamma M m1_v ->
-      evalma Gamma (p, tmtc ((pp,M,(dp,N)::sigma_v)::phi1_v), Delta, v)
+      evalma Gamma (p, emtc ((pp,M,(dp,N)::sigma_v)::phi1_v), Delta, v)
              ((map (fun tpl => match tpl with
-                              | (ttpl v11 v12) => map (fun t => let '(v1, (m1, Gamma1), p1) := t in (p1,m1,Gamma1,v1)) (zip3 [v11;v12] m1_v p1_v)
+                              | (etpl v11 v12) => map (fun t => let '(v1, (m1, Gamma1), p1) := t in (p1,m1,Gamma1,v1)) (zip3 [v11;v12] m1_v p1_v)
                               | v11 => map (fun t => let '(v1, (m1, Gamma1), p1) := t in (p1,m1,Gamma1,v1)) (zip3 [v11] m1_v p1_v)
                               end
                    ) v1_vv)) empty.
@@ -226,15 +251,15 @@ Module Egison.
   (* (match-all [1 2] unordered-pair {[<pair $a $b> [a b]]}) ===> {[1,2] [2,1]} *)
 
   Open Scope string_scope.
-  Definition unordered_pair: tm :=
-    (tmtc [(pppair ppdol ppdol, ttplmtc tsm tsm,
-            [(dppair (dpvar "x") (dpvar "y"), (tcll [(ttpl (tvar "x") (tvar "y")); ttpl (tvar "y") (tvar "x")]))]);
-           (ppdol, tsm,
-            [(dpvar "tgt", tcll [tvar "tgt"])])]).
+  Definition unordered_pair: exp :=
+    (emtc [(pppair ppdol ppdol, etplmtc esm esm,
+            [(dppair (dpvar "x") (dpvar "y"), (ecll [(etpl (evar "x") (evar "y")); etpl (evar "y") (evar "x")]))]);
+           (ppdol, esm,
+            [(dpvar "tgt", ecll [evar "tgt"])])]).
 
-  Definition match_all_example: tm :=
-    (tmal (tpair (tint 1) (tint 2)) unordered_pair (ppair (pvar "a") (pvar "b"),ttpl (tvar "a") (tvar "b"))).
-  Theorem unordered_pair_example : eval (empty, match_all_example, tcll [ttpl (tint 1) (tint 2);ttpl (tint 2) (tint 1)]).
+  Definition match_all_example: exp :=
+    (expmal (epair (eint 1) (eint 2)) unordered_pair (ppair (pvar "a") (pvar "b"),etpl (evar "a") (evar "b"))).
+  Theorem unordered_pair_example : eval (empty, match_all_example, ecll [etpl (eint 1) (eint 2);etpl (eint 2) (eint 1)]).
   Proof.
     econstructor.
     - repeat econstructor.
@@ -245,7 +270,7 @@ Module Egison.
         * repeat econstructor.
         * econstructor.
           -- econstructor.
-             eapply ema.
+             eapply Ema.
              --- repeat econstructor.
              --- repeat econstructor.
              --- repeat econstructor.
